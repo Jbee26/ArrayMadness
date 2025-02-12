7b31bd2d6a5c7f4e25b75d788fb5dd03f19d0a7e
Jbee26 <jack_belyea26@milton.edu>
2025-02-12 09:06:01 -0500

    :)

--- src/Arrays2D.java
+++ src/Arrays2D.java
@@ -4,18 +4,55 @@ public class Arrays2D {
     }
 
     int [][] grid = new int[3][4];
+    int sum = 0;
 
     public Arrays2D(){
         System.out.println("Welcome to Grid World!");
-        int random = (int)(Math.random()*101);
+
         System.out.println(grid.length);
         for(int r = 0; r < grid.length; r++){
             for(int c = 0; c < grid[r].length; c++){
+                int random = (int)(Math.random()*101);
+                grid[r][c] = random;
+
+            }
+
+        }
+        printGrid();
+        System.out.println(avgNum() + "= average ");
+
+    }
+
+    public void printGrid(){
+        for(int r = 0; r < grid.length; r++){
+            for(int c = 0; c < grid[r].length; c++){
+                System.out.println("(" + r +","+c+"): " +grid[r][c]);
+
 
             }
 
         }
 
     }
+    public double avgNum() {
 
-}
+        double average = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[r].length; c++) {
+                sum += grid[r][c];
+
+            }
+
+
+
+        }
+        average = (double) sum / (grid.length * grid[0].length);
+
+        return average;
+
+    }
+
+        // return the avg num that is in the grid
+    }
+
+
