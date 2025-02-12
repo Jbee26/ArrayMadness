3fbf3e5a0f2ca44c629059f473912083bf77e76e
Jbee26 <jack_belyea26@milton.edu>
2025-02-12 08:46:48 -0500

    :)

---
+++ src/Arrays2D.java
@@ -0,0 +1,21 @@
+public class Arrays2D {
+    public static void main(String[] args) {
+        new Arrays2D();
+    }
+
+    int [][] grid = new int[3][4];
+
+    public Arrays2D(){
+        System.out.println("Welcome to Grid World!");
+        int random = (int)(Math.random()*101);
+        System.out.println(grid.length);
+        for(int r = 0; r < grid.length; r++){
+            for(int c = 0; c < grid[r].length; c++){
+
+            }
+
+        }
+
+    }
+
+}
